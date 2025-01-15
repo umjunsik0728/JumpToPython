43cbf22f0d87fd0b6cbbec4107fe06a99cb37752
HSM <junsikgimmotti@gmail.com>
2025-01-15 22:10:51 +0900

    연습

--- 2장 자료형/2장연습.py
+++ 2장 자료형/2장연습.py
@@ -1,3 +1,83 @@
 ## 1번 문제
-arr = [80, 75, 55]
-print(f"average : {sum(arr) / len(arr)}")
+# arr = [80, 75, 55]
+# print(f"average : {sum(arr) / len(arr)}")
+
+## 2번 문제
+# n = 199993847
+# if n % 2 == 0:
+#     print(f"{n}은 짝수입니다.")
+# else:
+#     print(f"{n}은 홀수입니다.")
+
+## 3번 문제
+# 주민등록번호
+# jumin = "090728-192837"
+
+# # 연월일 부분 (YYYYMMDD)
+# year_month_day = jumin[:6]
+
+# # 그 뒤의 숫자 부분
+# remaining_numbers = jumin[7:]
+
+# print("연월일 부분:", year_month_day)
+# print("숫자 부분:", remaining_numbers)
+
+## 4번 문제
+# # 주민등록번호
+# jumin = "090728-292837"
+
+# # 뒷자리 숫자 중 첫 번째 숫자 (성별을 나타냄)
+# gender_digit = int(jumin[7])
+
+# # 성별 판별
+# if gender_digit % 2 == 1:
+#     gender = "남성"
+# else:
+#     gender = "여성"
+
+# print("성별:", gender)
+
+## 5번 문제
+# original_string = "a:b:c:d"
+# modified_string = original_string.replace(":", "#")
+
+# print(modified_string)
+
+## 6번 문제
+# original_list = [1, 3, 5, 4, 2]
+# reversed_list = original_list[::-1]
+
+# print(reversed_list)
+
+## 7번 문제
+# a = ['Life', 'is', 'too', 'short']
+# result = " ".join(a)
+
+# print(result)
+
+## 8번 문제
+# a = (1, 2, 3)
+# a = a+(4,)
+# print(a)
+
+## 9번 문제
+# a = dict()
+# a{}
+
+
+## 10번 문제 
+# a = {'A': 90, 'B': 80, 'c': 70}
+# result = a.pop('B')
+# print(a)
+# print(result)
+
+# ## 11번 문제 
+# a = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5]
+# aSet = set(a)
+# b = list(aSet)
+# print(b)
+
+# ## 12번 문제제
+# a = b = [1, 2, 3]
+# a[1] = 4
+# print(b)
